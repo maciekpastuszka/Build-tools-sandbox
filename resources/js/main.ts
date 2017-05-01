18f8a4dbb2a732d4ece0ab55ef9e71aedf46e7a4
Maciej <maciej.pastuszka@gmail.com>
2017-05-01 16:15:19 +0200

    add class pattern

--- resources/js/main.ts
+++ resources/js/main.ts
@@ -1,12 +1,23 @@
 interface Person {
     name: string;
-    age?: number;
+    age?: number; //optional
 }
 
-
-
-function getPerson(person: Person) {
+function getPerson(person: Person): void { //void = not return
    console.log(person.name);
 }
 
 getPerson({name: "Maciek", age: 27});
+
+class Greeter {
+    greeting: string;
+    constructor(message: string) {
+        this.greeting = message;
+    }
+    greet() {
+        console.log(`Hi ${this.greeting}`);
+    }
+}
+
+let greeter = new Greeter("world");
+greeter.greet();
--- resources/js/ts_temp/main.js
+++ resources/js/ts_temp/main.js
@@ -2,3 +2,14 @@ function getPerson(person) {
     console.log(person.name);
 }
 getPerson({ name: "Maciek", age: 27 });
+var Greeter = (function () {
+    function Greeter(message) {
+        this.greeting = message;
+    }
+    Greeter.prototype.greet = function () {
+        console.log("Hi " + this.greeting);
+    };
+    return Greeter;
+}());
+var greeter = new Greeter("world");
+greeter.greet();
